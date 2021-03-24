f5f459a5141b7f8e4cdcfe0f6a5eb37ba79b3a4f
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-03-24 18:55:31 +0100

    rearrange project structure and add first challenge

---
+++ app/javascript/plugins/script_handler.js
@@ -0,0 +1,15 @@
+const scriptNotFound = () => {};
+
+const loadChallengeScript = () => {
+  const challengeWrapper = document.getElementById("challenge-wrapper");
+  if (challengeWrapper) {
+    const challengeId = challengeWrapper.dataset.challengeId;
+    import(`./challenges/${challengeId}.js`)
+      .then((module) => {
+        module.challengeScript();
+      })
+      .catch(() => console.log("Script not found"));
+  }
+};
+
+export { loadChallengeScript };
