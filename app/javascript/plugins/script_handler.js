c1042aaae006b1c3463f51ea27438f058cd4dbc3
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-03-26 13:23:05 +0100

    add swiss railway clock effect and some documentation

--- app/javascript/plugins/script_handler.js
+++ app/javascript/plugins/script_handler.js
@@ -1,13 +1,13 @@
 const loadChallengeScript = () => {
   const challengeWrapper = document.getElementById("challenge-wrapper");
-  if (challengeWrapper) {
-    const challengeId = challengeWrapper.dataset.challengeId;
-    import(`./challenges/${challengeId}.js`)
-      .then((module) => {
-        module.challengeScript();
-      })
-      .catch(() => console.log("Script not found"));
-  }
+  if (!challengeWrapper) return;
+
+  const challengeId = challengeWrapper.dataset.challengeId;
+  import(`./challenges/${challengeId}.js`)
+    .then((module) => {
+      module.challengeScript();
+    })
+    .catch(() => console.log("Script not found"));
 };
 
 export { loadChallengeScript };
